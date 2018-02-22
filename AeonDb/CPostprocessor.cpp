39a8363859dd293e6d911634e02ccd216f6254d4
Jonathan Louie <johnlouie88@outlook.com>
2018-02-21 23:12:10 -0800

    Implemented CPostprocessor and CPreprocessor

--- AeonDb/CPostprocessor.cpp
+++ AeonDb/CPostprocessor.cpp
@@ -1,11 +1,76 @@
+//	CPostprocessor.cpp
+//
+//	CPostprocessor class
+//
+//	CPostprocessor is a container for postprocessing techniques for generating
+//	meaningful indexing terms. This class follows a mix and match style giving
+//	the indexing engine great flexibility to adjust to the numerous combinations
+//	of writing systems, grammar, whitespace structures, etc present in textual
+//	data. This allows which subset of indexing techniques to be selected at runtime,
+//	allowing end users to write configuration files or change in real time.
+//
+//	WARNING: The order in which concrete subclasses of IPostprocess is inserted is important.
+//
+//	CIndexEngine uses this class.
+
 #include "stdafx.h"
 
-CPostprocessor::CPostprocessor (void){ }
-CPostprocessor::CPostprocessor (const CPostprocessor &Other){ }
-CPostprocessor::~CPostprocessor (void){ }
+CPostprocessor::CPostprocessor (void)
+	
+//	CPostprocessor constructor
 
-CPostprocessor &CPostprocessor::operator= (const CPostprocessor &Other) { return *this; }
+	{
 
-void CPostprocessor::Append(IPostprocess *pProc) {}
+	//	Intentionally blank.
 
-void CPostprocessor::Run(CTermOccurenceStream &Data) {}
+	}
+
+CPostprocessor::CPostprocessor (const CPostprocessor &Other) : m_Processes(Other.m_Processes)
+
+//	CPostprocessor copy constructor
+
+	{
+
+	//	Intentionally blank.
+
+	}
+
+CPostprocessor::~CPostprocessor (void)
+	
+//	CPostprocessor destructor
+
+	{
+	m_Processes.DeleteAllAndFreeValues();
+	}
+
+CPostprocessor &CPostprocessor::operator= (const CPostprocessor &Other)
+
+//	CPostprocessor copy assignment operator
+
+	{
+	m_Processes = Other.m_Processes;
+	return *this;
+	}
+
+void CPostprocessor::Append (IPostprocess *pProc)
+
+//	Append
+//
+//	Inserts a postprocessing technique to the end. Insertion order is important.
+
+	{
+	m_Processes.Insert(pProc);
+	}
+
+void CPostprocessor::Run (CTermOccurenceStream &Data)
+
+//	Run
+//
+//	Processes the data using the selected postprocessing techniques in place.
+
+	{
+	for (int i = 0; i < m_Processes.GetCount(); i++)
+		{
+		m_Processes.GetAt(i)->Operation(Data);
+		}
+	}
--- AeonDb/CPreprocessor.cpp
+++ AeonDb/CPreprocessor.cpp
@@ -1,10 +1,76 @@
+//	CPreprocessor.cpp
+//
+//	CPreprocessor class
+//
+//	CPreprocessor is a container for preprocessing techniques for generating
+//	meaningful indexing terms. This class follows a mix and match style giving
+//	the indexing engine great flexibility to adjust to the numerous combinations
+//	of writing systems, grammar, whitespace structures, etc present in textual
+//	data. This allows which subset of indexing techniques to be selected at runtime,
+//	allowing end users to write configuration files or change in real time.
+//
+//	WARNING: The order in which concrete subclasses of IPreprocess is inserted is important.
+//
+//	CIndexEngine uses this class.
+
 #include "stdafx.h"
 
-CPreprocessor::CPreprocessor (void) { }
-CPreprocessor::CPreprocessor (const CPreprocessor &Other){ }
-CPreprocessor::~CPreprocessor (void){ }
+CPreprocessor::CPreprocessor (void)
 
-CPreprocessor &CPreprocessor::operator= (const CPreprocessor &Other) { return *this; }
+//	CPreprocessor constructor
 
-void CPreprocessor::Append(IPreprocess *pProc) {}
-void CPreprocessor::Run(CString &sData) {}
+	{
+
+	//	Intentionally blank.
+
+	}
+
+CPreprocessor::CPreprocessor (const CPreprocessor &Other) : m_Processes(Other.m_Processes)
+
+//	CPreprocessor copy constructor
+
+	{
+
+	//	Intentionally blank.
+
+	}
+
+CPreprocessor::~CPreprocessor (void)
+
+//	CPreprocessor destructor
+
+	{
+	m_Processes.DeleteAllAndFreeValues();
+	}
+
+CPreprocessor &CPreprocessor::operator= (const CPreprocessor &Other)
+
+//	CPreprocessor copy assignment operator
+
+	{
+	m_Processes = Other.m_Processes;
+	return *this;
+	}
+
+void CPreprocessor::Append (IPreprocess *pProc)
+
+//	Append
+//
+//	Inserts a preprocessing technique to the end. Insertion order is important.
+
+	{
+	m_Processes.Insert(pProc);
+	}
+
+void CPreprocessor::Run (CString &sData)
+
+//	Run
+//
+//	Processes the data using the selected preprocessing techniques in place.
+
+	{
+	for (int i = 0; i < m_Processes.GetCount(); i++)
+		{
+		m_Processes.GetAt(i)->Operation(sData);
+		}
+	}
